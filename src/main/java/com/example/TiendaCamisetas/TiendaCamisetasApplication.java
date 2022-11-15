307426ed6015707cf85e81f040e712d1541e717b
Danilo0528 <dpino131@gmail.com>
2022-11-15 15:58:29 -0500

    Creacion del proyecto

---
+++ src/main/java/com/example/TiendaCamisetas/TiendaCamisetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.TiendaCamisetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCamisetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCamisetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TiendaCamisetas/TiendaCamisetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TiendaCamisetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaCamisetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
