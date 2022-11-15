07fa4f8eab8daeb65ffde2f7a96d28b2eef08340
Danilo0528 <dpino131@gmail.com>
2022-11-15 17:24:10 -0500

    Creacion de la tabla usurios

---
+++ TiendaCamisetas/src/main/java/com/example/TiendaCamisetas/Modelo/Usuarios.java
@@ -0,0 +1,28 @@
+
+package com.example.TiendaCamisetas.Modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity  //reconoce las identidades en la tabla
+@Table (name = "usuarios") //crea la tabla usuarios
+@NoArgsConstructor     //constructor Vacio
+@AllArgsConstructor    //Constructor con todos los argumentos
+public class Usuarios {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    
+    @Column(name = "id")//con esto seleccionamos la columna de la tabla
+    private Integer id;
+    @Column (name = "nombre", length = 50)
+    private String nombre;
+    @Column (name ="contrseña", length = 50)
+    private int contraseña;
+    
+}
