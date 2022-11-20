ebcce5a06411a61b27575153a480f27363aeb4c9
Danilo0528 <dpino131@gmail.com>
2022-11-20 10:15:33 -0500

    Pequeñas modificasiones de bugs y errores

--- TiendaCamisetas/src/main/java/com/example/TiendaCamisetas/Model/Productos.java
+++ TiendaCamisetas/src/main/java/com/example/TiendaCamisetas/Model/Productos.java
@@ -103,7 +103,6 @@ public class Productos implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Productos)) {
             return false;
         }
--- TiendaCamisetas/src/main/java/com/example/TiendaCamisetas/Model/Usuarios.java
+++ TiendaCamisetas/src/main/java/com/example/TiendaCamisetas/Model/Usuarios.java
@@ -92,7 +92,7 @@ public class Usuarios implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        
         if (!(object instanceof Usuarios)) {
             return false;
         }
